8baf550c65b0c1de6ed1b407af90101c29bfa2bb
YoungChulShin <go1323@gmail.com>
2021-10-09 08:17:14 +0900

    Get 메서드 구현

--- first_rest_api/src/main/kotlin/study/spring/kotlin/firstrestapi/FirstrestapiApplication.kt
+++ first_rest_api/src/main/kotlin/study/spring/kotlin/firstrestapi/FirstrestapiApplication.kt
@@ -2,6 +2,8 @@ package study.spring.kotlin.firstrestapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class FirstrestapiApplication
@@ -9,3 +11,16 @@ class FirstrestapiApplication
 fun main(args: Array<String>) {
     runApplication<FirstrestapiApplication>(*args)
 }
+
+@RestController
+class MessageResource {
+    @GetMapping
+    fun index(): List<Message> = listOf(
+        Message("1", "Hello!"),
+        Message("2", "Bonjour!"),
+        Message("3", "Privet!")
+    )
+}
+
+data class Message(val id: String?, val text: String)
+
