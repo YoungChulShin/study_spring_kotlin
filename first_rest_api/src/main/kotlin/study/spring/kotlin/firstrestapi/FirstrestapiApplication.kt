bbdfd7905a3986953e9d50d6ec0e9beea13f0f9b
YoungChulShin-Russel <youngchul.shin@meshkorea.net>
2021-10-08 22:40:01 +0900

    프로젝트 초안

---
+++ first_rest_api/src/main/kotlin/study/spring/kotlin/firstrestapi/FirstrestapiApplication.kt
@@ -0,0 +1,11 @@
+package study.spring.kotlin.firstrestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstrestapiApplication
+
+fun main(args: Array<String>) {
+    runApplication<FirstrestapiApplication>(*args)
+}
