05b006705d6f44839fdd79583f6a01ae44011299
YoungChulShin <go1323@gmail.com>
2021-10-09 08:43:20 +0900

    DB 저장 메서드 구현

---
+++ first_rest_api/README.md
@@ -0,0 +1,2 @@
+# 참고 사이트
+https://kotlinlang.org/docs/basic-syntax.html#program-entry-point
--- first_rest_api/src/main/kotlin/study/spring/kotlin/firstrestapi/FirstrestapiApplication.kt
+++ first_rest_api/src/main/kotlin/study/spring/kotlin/firstrestapi/FirstrestapiApplication.kt
@@ -2,7 +2,14 @@ package study.spring.kotlin.firstrestapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
@@ -13,14 +20,36 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class MessageResource {
+class MessageResource(val service: MessageService) {
+
     @GetMapping
-    fun index(): List<Message> = listOf(
-        Message("1", "Hello!"),
-        Message("2", "Bonjour!"),
-        Message("3", "Privet!")
-    )
+    fun index(): List<Message> {
+        return service.findMessage();
+    }
+
+    @PostMapping
+    fun post(@RequestBody message: Message): String {
+        service.post(message);
+        return message.text;
+    }
 }
 
-data class Message(val id: String?, val text: String)
+@Service
+class MessageService(val db: MessageRepository) {
+
+    fun findMessage() : List<Message> = db.findMessages();
+
+    fun post(message: Message) {
+        db.save(message);
+    }
+}
+
+interface MessageRepository : CrudRepository<Message, String> {
+
+    @Query("select * from messages")
+    fun findMessages() : List<Message>
+}
+
+@Table("MESSAGES")
+data class Message(@Id val id: String?, val text: String)
 
