61fb722bac7c9903ec4aa64a07f22ab5d716875d
YoungChulShin <go1323@gmail.com>
2021-10-11 08:05:00 +0900

    entity, repository 구현

--- blog/README.md
+++ blog/README.md
@@ -1,2 +1,12 @@
 # 참고 사이트
 https://spring.io/guides/tutorials/spring-boot-kotlin/
+
+# 메모
+Extension 메서드 생성 시 class 없이 생성
+- java에서는 어떻게 처리되는거지?
+
+JPA entity를 만들 때는 var로 만들어야한다
+```
+Here we don’t use data classes with val properties because JPA is not designed to work with immutable classes or the methods generated automatically by data classes. If you are using other Spring Data flavor, most of them are designed to support such constructs so you should use classes like data class User(val login: String, …​) when using Spring Data MongoDB, Spring Data JDBC, etc.
+```
+
---
+++ blog/src/main/kotlin/study/spring/kotlin/blog/Entities.kt
@@ -0,0 +1,25 @@
+package study.spring.kotlin.blog
+
+import javax.persistence.ManyToOne
+import java.time.LocalDateTime
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity
+class Article(
+    var title: String,
+    var headline: String,
+    var conent: String,
+    @ManyToOne var author: User,
+    var slug: String = title.toSlug(),
+    var addedAt: LocalDateTime = LocalDateTime.now(),
+    @Id @GeneratedValue var id: Long? = null)
+
+@Entity
+class User (
+    var login: String,
+    var firstname: String,
+    var lastname: String,
+    var description: String? = null,
+    @Id @GeneratedValue var id: Long? = null)
