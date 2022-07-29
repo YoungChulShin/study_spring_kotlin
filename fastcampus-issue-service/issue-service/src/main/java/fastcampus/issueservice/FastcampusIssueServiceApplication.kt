0dc2fae31af9eec5a8d5011a9d00660cdea8818b
YoungChulShin <go1323@gmail.com>
2022-07-30 00:03:05 +0900

    issue-service 프로젝트 추가 및 기본설정

--- fastcampus-issue-service/issue-service/src/main/java/fastcampus/issueservice/FastcampusIssueServiceApplication.kt
+++ fastcampus-issue-service/issue-service/src/main/java/fastcampus/issueservice/FastcampusIssueServiceApplication.kt
@@ -1,4 +1,4 @@
-package study.spring.kotlin.fastcampusissueservice
+package fastcampus.issueservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
