adf030b5c16fa5ebc272597874d1fee7ff9a0bad
YoungChulShin <go1323@gmail.com>
2022-07-29 23:41:50 +0900

    프로젝트 초안

---
+++ fastcampus-issue-service/src/main/kotlin/study/spring/kotlin/fastcampusissueservice/FastcampusIssueServiceApplication.kt
@@ -0,0 +1,11 @@
+package study.spring.kotlin.fastcampusissueservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FastcampusIssueServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<FastcampusIssueServiceApplication>(*args)
+}
---
+++ fastcampus-issue-service/src/test/kotlin/study/spring/kotlin/fastcampusissueservice/FastcampusIssueServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.spring.kotlin.fastcampusissueservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FastcampusIssueServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
