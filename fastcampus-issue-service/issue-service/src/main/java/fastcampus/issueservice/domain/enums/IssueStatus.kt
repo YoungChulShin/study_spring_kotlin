32d84e048984d647aa52e9c019f445a3a0f608ed
YoungChulShin <go1323@gmail.com>
2022-08-01 22:42:32 +0900

    domain 클래스 추가

---
+++ fastcampus-issue-service/issue-service/src/main/java/fastcampus/issueservice/domain/enums/IssueStatus.kt
@@ -0,0 +1,14 @@
+package fastcampus.issueservice.domain.enums
+
+enum class IssueStatus {
+
+    TODO,
+    IN_PROGRESS,
+    RESOLVED;
+
+    companion object {
+
+        operator fun invoke(status: String) = valueOf(status.uppercase())
+    }
+
+}
