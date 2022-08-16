ce5f915c1aab99185c699766dba471fdbb8add49
YoungChulShin <go1323@gmail.com>
2022-08-16 23:14:54 +0900

    SecurityConfig 추가

--- jwt_security/README.md
+++ jwt_security/README.md
@@ -2,3 +2,6 @@
 스프링 시큐리티 + JWT 토큰 발행 공부
 
 https://www.youtube.com/watch?v=VVn9OG9nfH0&t=3813s
+
+# 참고 문서
+https://www.baeldung.com/spring-deprecated-websecurityconfigureradapter
--- jwt_security/src/main/kotlin/study/spring/security/jwt_security/config/security/SecurityConfig.kt
+++ jwt_security/src/main/kotlin/study/spring/security/jwt_security/config/security/SecurityConfig.kt
@@ -2,22 +2,36 @@ package study.spring.security.jwt_security.config.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
+//@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize 어노테이션 활성화
 class SecurityConfig {
-
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http.csrf().disable()
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        http.authorizeHttpRequests().antMatchers("/api/login/**").permitAll()
+        http.authorizeHttpRequests()
+            .antMatchers("/api/login/**")
+            .permitAll()
+            .antMatchers(HttpMethod.GET, "/api/user/**")
+            .hasAnyAuthority("ROLE_USER")
+            .antMatchers(HttpMethod.POST, "/api/user/save/**")
+            .hasAnyAuthority("ROLE_ADMIN")
+            .anyRequest()
+            .authenticated()
 
         return http.build()
     }
@@ -25,6 +39,16 @@ class SecurityConfig {
     @Bean
     fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 
-    
+    @Bean
+    fun authenticationManager(
+        http: HttpSecurity,
+        userDetailsService: UserDetailsService,
+        passwordEncoder: PasswordEncoder): AuthenticationManager {
+        return http.getSharedObject(AuthenticationManagerBuilder::class.java)
+            .userDetailsService(userDetailsService)
+            .passwordEncoder(passwordEncoder)
+            .and()
+            .build()
+    }
 
 }
