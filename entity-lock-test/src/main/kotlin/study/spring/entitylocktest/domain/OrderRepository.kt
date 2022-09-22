5d1c9ff101bee322cff33b7ff8dcb8d7d39300a9
YoungChulShin <go1323@gmail.com>
2022-09-23 08:14:36 +0900

    Pessimistic lock을 이용한 Order 조회 추가

--- entity-lock-test/src/main/kotlin/study/spring/entitylocktest/domain/OrderRepository.kt
+++ entity-lock-test/src/main/kotlin/study/spring/entitylocktest/domain/OrderRepository.kt
@@ -1,5 +1,14 @@
 package study.spring.entitylocktest.domain
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import javax.persistence.LockModeType
 
-interface OrderRepository : JpaRepository<Order, Long>
+interface OrderRepository : JpaRepository<Order, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select o from Order o where o.id = :id")
+    fun findByIdForUpdate(@Param("id") id: Long): Order
+}
