c4dbcd19a69c52c9c126499d21e31ac231c5ad20
YoungChulShin <go1323@gmail.com>
2022-09-23 08:06:24 +0900

    프로젝트 추가

---
+++ entity-lock-test/src/test/kotlin/study/spring/entitylocktest/EntityLockTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.spring.entitylocktest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EntityLockTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
